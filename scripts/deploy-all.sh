58238ee3a532bb598a9fafc3a6b5e5411c72d245
Bryan Cisneros <genecc4@gmail.com>
2026-05-31 21:30:44 -0600

    fix(fly): fix app name and deploy script

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-flyctl deploy backend --config backend/fly.toml
-flyctl deploy frontend --config frontend/fly.toml
+flyctl deploy --config backend/fly.toml -a macso-patient-dream-1724 backend
+flyctl deploy --config frontend/fly.toml -a ionic-react-quiz-app frontend
